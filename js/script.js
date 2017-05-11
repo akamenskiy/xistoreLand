d4b98d512e83ef6e7c4b8d52345eb0e4d2531cfe
sashka <kamenskogo@gmail.com>
2017-05-11 17:18:00 +0300

    do desktop

--- js/script.js
+++ js/script.js
@@ -17,14 +17,5 @@ $(document).ready(function () {
             }
         ]
     });
-
-    $("#input__phone").keydown(function(e) {
-        var oldvalue=$(this).val();
-        var field=this;
-        setTimeout(function () {
-            if(field.value.indexOf('+375') !== 0) {
-                $(field).val(oldvalue);
-            }
-        }, 1);
-    });
 });
+
