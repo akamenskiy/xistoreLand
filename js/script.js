11e5730527180dabe6aa9f67a8ff8fe8a8826ced
sashka <kamenskogo@gmail.com>
2017-05-13 18:04:58 +0300

    do spare mobile

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@
  * Created by sashka on 06.05.17.
  */
 $(document).ready(function () {
+
+    $('.contacts_list__about').slick({
+        mobileFirst: true,
+        arrow: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        accessibility: true,
+        respondTo: "min",
+        responsive: [
+            {
+                breakpoint: 731,
+                settings: "unslick"
+            }
+        ]
+    });
+
     $('.gallery_slider').slick({
         arrows: true,
         slidesToShow: 4,
@@ -11,11 +27,17 @@ $(document).ready(function () {
             {
                 breakpoint: 1259,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 732,
+                settings: {
+                    slidesToShow: 1
                 }
             }
         ]
     });
+
 });
 
